699080921c569c9eda85d15bcf3a60b01a46127a
Jason L. van Brackel <jason@vanbrackel.net>
2014-02-22 12:41:13 -0500

    Fix start-selenium-grid.ps1

--- servers/start-selenium-grid.ps1
+++ servers/start-selenium-grid.ps1
@@ -1,2 +1,1 @@
-$env:calculatorcodehome = "C:\code\SouthFlordidaCodeCamp\SouthFloridaCodeCamp2014"
-java -jar .\servers\selenium-server-standalone-2.39.0.jar -hub
+java -jar .\selenium-server-standalone-2.39.0.jar -hub
