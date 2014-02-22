80db9fc8f2a206f7c3c3eb562110994de4931eb1
Jason L. van Brackel <jason@vanbrackel.net>
2014-02-22 10:20:57 -0500

    Modify build files to work around zip bug

--- build/build-calculator.bat
+++ build/build-calculator.bat
@@ -1,2 +1,13 @@
 ECHO "Building calculator"
-acs newpackage -B -Config Debug -Sln .\src\CalculatorDemo\CalculatorDemo.sln -O .\bin\Calculator.zip
+rd build\Calculator /S /Q
+mkdir build\Calculator
+mkdir build\Calculator\interfaces
+mkdir build\Calculator\interfaces\root
+msbuild .src\CalculatorDemo\CalculatorDemo.sln
+xcopy src\calculatordemo\calculatordemo\*.* build\Calculator\interfaces\root /S /-Y
+copy src\calculatordemo\DeploymentManifest.xml build\Calculator /Y
+del build\Calculator.zip
+cd build\Calculator
+"C:\Program Files\7-Zip\7z.exe" a -tZip -y ../../build\Calculator.zip
+cd ..
+cd ..
