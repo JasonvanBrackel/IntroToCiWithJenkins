03378f8b078325d5bfa46b96835607e795472f8f
Jason L. van Brackel <jason@vanbrackel.net>
2014-02-22 10:46:58 -0500

    7zip testing

--- build/build-calculator.bat
+++ build/build-calculator.bat
@@ -4,10 +4,10 @@ mkdir build\Calculator
 mkdir build\Calculator\interfaces
 mkdir build\Calculator\interfaces\root
 msbuild .src\CalculatorDemo\CalculatorDemo.sln
-xcopy src\calculatordemo\calculatordemo\*.* build\Calculator\interfaces\root /S /-Y
+xcopy src\calculatordemo\calculatordemo\*.* build\Calculator\interfaces\root /I /S /-Y
 copy src\calculatordemo\DeploymentManifest.xml build\Calculator /Y
 del build\Calculator.zip
 cd build\Calculator
-"C:\Program Files\7-Zip\7z.exe" a -tZip -y build\Calculator.zip "C:\Program Files (x86)\Jenkins\jobs\South Florida Code Camp Demo\workspace\build\Calculator\*"
+"C:\Program Files\7-Zip\7z.exe" a -tZip -y build\Calculator.zip build\Calculator\*"
 cd ..
 cd ..
