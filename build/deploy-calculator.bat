0a83c29fe61b0c0d14f6f245ba6be6806e9fc7b9
Jason L. van Brackel <jason@vanbrackel.net>
2014-02-22 10:24:32 -0500

    Fix deploy script

--- build/deploy-calculator.bat
+++ build/deploy-calculator.bat
@@ -1,4 +1,4 @@
 ECHO "Removing application calculator"
 acs removeapplication -AppAlias calculator
 ECHO "Deploying application calculator"
-acs newapplication -AppAlias calculator -AppName calculator -Package .\bin\Calculator.zip -Stage sandbox
+acs newapplication -AppAlias calculator -AppName calculator -Package .\build\Calculator.zip -Stage sandbox
