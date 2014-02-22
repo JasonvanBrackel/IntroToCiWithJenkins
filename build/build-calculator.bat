d6abce691a66332bf44c01ec2a8a8f05a6407d2f
Jason L. van Brackel <jason@vanbrackel.net>
2014-02-22 10:49:06 -0500

    script fixes

--- build/build-calculator.bat
+++ build/build-calculator.bat
@@ -1,13 +1,13 @@
 ECHO "Building calculator"
-rd build\Calculator /S /Q
-mkdir build\Calculator
-mkdir build\Calculator\interfaces
-mkdir build\Calculator\interfaces\root
-msbuild .src\CalculatorDemo\CalculatorDemo.sln
-xcopy src\calculatordemo\calculatordemo\*.* build\Calculator\interfaces\root /I /S /-Y
-copy src\calculatordemo\DeploymentManifest.xml build\Calculator /Y
-del build\Calculator.zip
-cd build\Calculator
+rd .\build\Calculator /S /Q
+mkdir .\build\Calculator
+mkdir .\build\Calculator\interfaces
+mkdir .\build\Calculator\interfaces\root
+msbuild .\src\CalculatorDemo\CalculatorDemo.sln
+xcopy .\src\calculatordemo\calculatordemo\*.* .\build\Calculator\interfaces\root /I /S /-Y
+copy .\src\calculatordemo\DeploymentManifest.xml .\build\Calculator /Y
+del .\build\Calculator.zip
+cd .\build\Calculator
 "C:\Program Files\7-Zip\7z.exe" a -tZip -y build\Calculator.zip build\Calculator\*"
 cd ..
 cd ..
