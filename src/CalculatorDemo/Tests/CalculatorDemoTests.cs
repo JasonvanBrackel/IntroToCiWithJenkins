0f46d5e76d5c787928de356793eebb0b3d228d7d
Jonathan Newell <jonathananewell@hotmail.com>
2014-08-22 11:00:59 -0400

    Update CalculatorDemoTests.cs

--- src/CalculatorDemo/Tests/CalculatorDemoTests.cs
+++ src/CalculatorDemo/Tests/CalculatorDemoTests.cs
@@ -10,10 +10,11 @@ namespace Tests
     {
         protected IWebDriver Driver { get; set; }
 
+        // change this URL to your root site/diredctory port number    
         [SetUp]
         public void OpenCalculator()
         {
-            Driver.Url = "http://apps.apprenda.jason/calculator--v1/";
+            Driver.Url = "http://localhost:57568/Home/Index/";
         }
 
         [Test]
