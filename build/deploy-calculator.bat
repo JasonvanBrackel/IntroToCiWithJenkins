789ce2eeee587286a2b9f22d2ce5b7c7d21f6c98
Jason van Brackel <jvanbrackel@apprenda.com>
2015-06-16 22:36:36 -0400

    Update PowerPoint slides and remove superfluous deploy script

--- build/deploy-calculator.bat
+++
@@ -1,4 +0,0 @@
-ECHO "Removing application calculator"
-acs removeapplication -AppAlias calculator
-ECHO "Deploying application calculator"
-acs newapplication -AppAlias calculator -AppName calculator -Package .\build\Calculator.zip -Stage sandbox
