0b00a4859246cbd93ec3d0fec90364ba30301343
Jason van Brackel <jvanbrackel@apprenda.com>
2015-10-10 12:19:45 -0400

    Remove IE tests for now

--- src/CalculatorDemo/Tests/CalculatorDemoTests.cs
+++ src/CalculatorDemo/Tests/CalculatorDemoTests.cs
@@ -214,11 +214,11 @@ namespace Tests
         }
     }
 
-    public class IETests : CalculatorDemoTests
-    {
-        public IETests()
-        {
-            Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), DesiredCapabilities.InternetExplorer());
-        }
-    }
+    //public class IETests : CalculatorDemoTests
+    //{
+    //    public IETests()
+    //    {
+    //        Driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), DesiredCapabilities.InternetExplorer());
+    //    }
+    //}
 }
