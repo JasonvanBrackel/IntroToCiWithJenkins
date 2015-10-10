c063460d3aa4233784477efc444da0aa71cd0286
Jason van Brackel <jvanbrackel@apprenda.com>
2015-10-10 11:39:49 -0400

    Add build and package for deployment batch files

---
+++ build/build-calculator.bat
@@ -0,0 +1,11 @@
+ECHO "Building calculator"
+rd .\build\Calculator /S /Q
+mkdir .\build\Calculator\interfaces\root
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\src\CalculatorDemo\CalculatorDemo.sln
+xcopy .\src\calculatordemo\calculatordemo\*.* .\build\Calculator\interfaces\root /I /S /-Y
+copy .\src\calculatordemo\DeploymentManifest.xml .\build\Calculator /Y
+del .\build\Calculator.zip
+cd .\build\Calculator
+"C:\Program Files\7-Zip\7z.exe" a -tZip -y ..\Calculator.zip .\*
+cd ..
+cd ..
---
+++ build/deploy-calculator.bat
@@ -0,0 +1,6 @@
+﻿acs registercloud -Url https://apps.apprenda.mck603 -alias mck
+acs connectcloud -cloudalias mck -user jason@vanbrackel.net -password apprendaadmin -Remember
+ECHO "Removing application calculator"
+acs removeapplication -AppAlias calculator
+ECHO "Deploying application calculator"
+acs newapplication -AppAlias calculator -AppName calculator -Package .\build\Calculator.zip -Stage sandbox
